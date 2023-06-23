4cb01367fdc95976fa9c5e199be4bd832afbdf30
Menglot Kheng <you@example.com>
2023-06-23 08:09:48 +0700

    init

--- src/main/java/kh/com/kshrd/docengine/DocEngineApplication.java
+++ src/main/java/kh/com/kshrd/docengine/DocEngineApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @SpringBootApplication
 @EnableScheduling
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer")
